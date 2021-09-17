fc3905fd44b22e416d42689519dafb71e55063a0
Clupai 8o0 <clupaio4@gmail.com>
2021-09-17 21:24:11 +0300

    just got rid of .jpg stuff and all

--- playground/index.js
+++ playground/index.js
@@ -1,4 +1,19 @@
-const validator = require("validator");
 const colors = require("colors");
 
-console.log(validator.isEmail("limbusamaka@icloud.com"))
+const file1 = "challenge.jpg";
+const file2 = "challenge.png";
+const file3 = "challenge.jpeg";
+
+let concat = 0;
+
+const test = (file) => {
+  if (
+    file.slice(file.length - 3, file.length) === "jpg" ||
+    file.slice(file.length - 3, file.length) === "png"
+  )
+    concat = 3;
+  else if (file.slice(file.length - 4, file.length) === "jpeg") concat = 4;
+}
+test(file3);
+console.log(colors.green(file1.slice(0, file1.length-concat)));
+// console.log(colors.green(file.slice(file.length-3, file.length)))
