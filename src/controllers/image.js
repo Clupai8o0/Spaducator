e56b3173c998cd0fe588c4379f442c41630e92f4
Dhruv <dhruvnation15@gmail.com>
2021-09-17 21:18:12 +0300

    fix

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -19,19 +19,14 @@ imageRouter.route('/image/:filename')
 .get((req, res, next) => {
     gfs.find({ filename: req.params.filename }).toArray((err, files) => {
         if (!files[0] || files.length === 0) {
-            return res.status(200).json({
-                success: false,
-                message: 'No files available',
-            });
+            return res.status(404).send("Not found");
         }
 
         if (files[0].contentType === 'image/jpeg' || files[0].contentType === 'image/png' || files[0].contentType === 'image/svg+xml') {
             // render image to browser
             gfs.openDownloadStreamByName(req.params.filename).pipe(res);
         } else {
-            res.status(404).json({
-                err: 'Not an image',
-            });
+            res.status(400).send("Bad Request , Not an image");
         }
     });
 });
